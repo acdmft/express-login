821bc4b3538a007e1272fdd059cd21bb0fa91239
fusbudget <andrewsanser@gmail.com>
2022-03-28 16:41:24 +0200

    add test signup form in Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
 export default function Signup() {
+
+  const onChangeName = () => {
+
+  }
+
+  const onChangePwd = () => {
+
+  }
+
   return (
-    <div>Signup</div>
+    <>
+      <div>Signup</div>
+      <div>
+        <form >
+          <div>
+            <label htmlFor="name">Username: </label>
+            <input type="name" id="name" name="name" placeholder="username" onChange={onChangeName} required />
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password" name="password" placeholder="password" onChange={onChangePwd} required />
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
