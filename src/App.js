f9af17c41a08a1820a30379e0f01fcb90311f70e
fusbudget <andrewsanser@gmail.com>
2022-03-28 14:54:26 +0200

    add react-router-dom and routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+// Pages
+import Login from './pages/Login';
+import Admin from './pages/Admin';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div>App</div>
+        
+      <Switch>
+        <Route exact path="/login" component={Login}/> 
+        <Route path="/admin" component={Admin}/>
+        <Route path="/signup" component={Signup}/>
+      </Switch>
+
+    </BrowserRouter>
+
   )
 }
